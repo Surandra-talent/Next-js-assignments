42a312aeb1ba2d26f631ea22024604f10c268692
Surandra-talent <surandratlgt@gmail.com>
2024-03-28 10:53:19 +0530

    midddleware with protected routes on frontend

---
+++ src/middleware.js
@@ -0,0 +1,29 @@
+// import { NextResponse } from "next/server";
+
+// export function middleware(request) {
+//   //   if (request.nextUrl.pathname != "/login") {
+//   //     return NextResponse.redirect(new URL("/login", request.url));
+//   // }
+//   return NextResponse.redirect(new URL("/login", request.url));
+// }
+
+// export const config = {
+//   matcher: ["/dashboard/:path*", "/userslist/:path*"],
+// };
+
+import { NextResponse } from "next/server";
+
+export function middleware(request) {
+  const loggedIn = false;
+  const { pathname } = new URL(request.url);
+
+  if (!loggedIn && pathname !== "/login") {
+    return NextResponse.redirect(new URL("/login", request.url).toString());
+  }
+
+  return null; // Proceed with the request
+}
+
+export const config = {
+  middleware: true,
+};
